8d7c48087848da2feeaf6affc13f04f33fb0ece7
racer <mggwyx@gmail.com>
2022-05-23 14:48:33 +0800

    add code for c2

--- FSharpVersion/Chapter2.fs
+++ FSharpVersion/Chapter2.fs
@@ -1,13 +1,101 @@
 module FSharpVersion.Chapter2
 
+open Microsoft.ML.Probabilistic.Models
+open Microsoft.ML.Probabilistic.FSharp
+open Microsoft.ML.Probabilistic.Models.Attributes
+
 open MBMLViews
 open AssessingPeoplesSkills
+open FSharpVersion.Common
 
 [<Literal>]
 let DataPath =
     __SOURCE_DIRECTORY__
     + "/../src/2. Assessing Peoples Skills/Data/"
 
-let Infer() =
-    let input3 = FileUtils.Load<Inputs>(DataPath, "Toy3")
+let AddNoise (hasSkills: Variable<bool>) (probNoMistake: Variable<float>) (probGuess: Variable<float>) =
+    let mutable noisyAllTrue =
+        Variable.New<bool>().Named("NoisyAllTrueGated")
+
+    Variable.IfBlock
+        hasSkills
+        (fun _ ->
+            noisyAllTrue.SetTo(
+                Variable
+                    .Bernoulli(probNoMistake)
+                    .Named("probIfAllTrue")
+            ))
+        (fun _ ->
+            noisyAllTrue.SetTo(
+                Variable
+                    .Bernoulli(probGuess)
+                    .Named("probIfNotAllTrue")
+            ))
+
+    noisyAllTrue
+
+
+let NoisyAllTrue (variableArray: VariableArray<bool>) (probNoMistake: Variable<float>) (probGuess: Variable<float>): Variable<bool> =
+    let hasSkills =
+        Variable.AllTrue(variableArray).Named("hasSkills")
+    AddNoise hasSkills probNoMistake probGuess
+
+let ToyWith3Q2S () =
+    let numPeople, People = GetRange "People"
+
+    let numSkills, Skills = GetRange "Skills"
+
+    let numQuestions, Questions =
+        GetRange "Questions"
+
+    let probGuess =
+        GetVarArray<float> "probGuess" Questions
+
+    let probNotMistake =
+        GetVarArray<float> "probNoMistake" Questions
+
+    let probSkillTrue =
+        GetVarArray<float> "probSkillTrue" Skills
+
+    let numSkillsForEachQuestion =
+        (GetVarArray<int> "numSkillsForQuestions" Questions)
+            .Attrib(DoNotInfer())
+
+    let QuestionsSkills =
+        Range(numSkillsForEachQuestion[Questions])
+            .Named("questionsXskills")
+
+    let skillsNeeded =
+        Variable
+            .Array<VariableArray<int>, int [] []>(Variable.Array<int>(QuestionsSkills), Questions)
+            .Named("skillsNeeded")
+            .Attrib(DoNotInfer())
+
+    let skillsQuestionsMask =
+        Variable
+            .Array<VariableArray<bool>, int [] []>(Variable.Array<bool>(Skills), Questions)
+            .Named("skillsQuestionsMask")
+            .Attrib(DoNotInfer())
+
+    let skill =
+        Variable
+            .Array<VariableArray<bool>, int [] []>(Variable.Array<bool>(Skills), People)
+            .Named("skill")
+
+    skill.[People][Skills] <- Variable
+        .Bernoulli(probSkillTrue[Skills])
+        .ForEach(People)
+
+    let isCorrect =
+        Variable
+            .Array<VariableArray<bool>, int [] []>(Variable.Array<bool>(Questions), People)
+            .Named("isCorrect")
+
+    ()
+
+
+let Infer () =
+    let input3 =
+        FileUtils.Load<Inputs>(DataPath, "Toy3")
+
     printfn $"%A{input3}"
--- FSharpVersion/Common.fs
+++ FSharpVersion/Common.fs
@@ -1,6 +1,20 @@
 module FSharpVersion.Common
 
 open Microsoft.ML.Probabilistic.Models
+open Microsoft.ML.Probabilistic.FSharp
+open Microsoft.ML.Probabilistic.Models.Attributes
 
 let engine = InferenceEngine()
 engine.ShowFactorGraph <- true
+
+let GetRangeLen name =
+    Variable
+        .New<int>()
+        .Named(name)
+        .Attrib(DoNotInfer())
+
+let GetRange (name: string) =
+    let len = GetRangeLen("num" + name)
+    len, Range(len).Named(name)
+
+let GetVarArray<'T> name (range: Range) = Variable.Array<'T>(range).Named(name)
